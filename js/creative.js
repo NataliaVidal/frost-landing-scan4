28d7032a4800b2ff8aec2dfcd0bfc3b1b121b5e9
Natalia VIdal <natalia.vidal@inria.cl>
2021-03-04 13:40:24 -0300

    ScrollSpy to active nav tabs when scrolling

--- js/creative.js
+++ js/creative.js
@@ -46,3 +46,24 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+// ScrollSpy to make active nav tabs when scrolling through sections
+window.addEventListener('DOMContentLoaded', () => {
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+			}
+		});
+	});
+
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+
+});
